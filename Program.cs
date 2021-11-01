5532a535535cb945bda8d8e703ced63985db3d3b
Bahulya <sarvalabahulya456@gmail.com>
2021-11-01 22:07:01 +0530

    FlipTheCoin

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CoinFlip
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Label: Console.WriteLine("1.print the percentage coin Flip");
+            Console.WriteLine("Type the options");
+            int i = int.Parse(Console.ReadLine());
+
+            switch (i)
+            {
+                case 1:
+                    Console.WriteLine("1.Print the percentage coin Flip");
+                    percentageOfHeadAndTail percentageOfHeadAndTail = new percentageOfHeadAndTail();
+                        percentageOfHeadAndTail.printHeadAndTail();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+            Console.WriteLine("Do you want to continue...yes/No");
+            if (Console.ReadKey().KeyChar.ToString() == "yes")
+            {
+                goto Label;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+     
---
+++ percentageOfHeadAndTail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoinFlip
+{
+    public class percentageOfHeadAndTail
+    {
+
+        public void printHeadAndTail()
+        {
+            int head = 0;
+            int tail = 0;
+            int coinresult = 0;
+
+            Random random = new Random();
+
+            Console.WriteLine("Enter the input of coin Flip");
+
+            int readInputromuserNum = int.Parse(Console.ReadLine());
+            for (int i = 0; i < readInputromuserNum; i++)
+            {
+                coinresult = random.Next(0, 2);
+                if (coinresult == 0)
+                {
+                    head++;
+                }
+                else
+                {
+                    tail++;
+                }
+            }
+
+
+
+
+        }
+
+    }
+}
+
